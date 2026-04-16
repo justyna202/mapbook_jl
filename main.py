ef7e7a23e377b5906e52e5328178fedf0dce9a31
justyna202 <justyna.latkowska@student.wat.edu.pl>
2026-04-16 14:24:58 +0200

    wip: dynamic menu

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from mapbook_lib.model import  users
 from mapbook_lib.controller import  read_data
 
-read_data(users[1:])
+while True:
+    print('0 - zakończ program')
+    print('1 - wyświetl znajomych')
+    choose = input('wybierz opcję: ')
+    if choose =='0':
+        break
+    if choose == '1':
+        read_data(users[1:])

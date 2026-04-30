cd72a101a7a4a4462ed92a54af54c4bad852a38a
justyna202 <justyna.latkowska@student.wat.edu.pl>
2026-04-30 13:42:22 +0200

    feat: add update function

--- mapbook_lib/main.py
+++ mapbook_lib/main.py
@@ -1,14 +1,15 @@
-from mapbook_lib.model import  users
-from mapbook_lib.controller import  read_data, add_user, remove_user
+from mapbook_lib.model import users
+from mapbook_lib.controller import read_data, add_user, remove_user, update_user #wołanie funkcji
 
 while True:
     print('0 - zakończ program')
     print('1 - wyświetl znajomych')
     print('2 - dodaj znajomego')
     print('3 - usuń znajomego')
-    choose = input('wybierz opcję: ')
+    print('4 - zmień dane znajomego')
 
-    if choose =='0':
+    choose = input('wybierz opcję: ')
+    if choose == '0':
         break
     if choose == '1':
         read_data(users[1:])
@@ -16,3 +17,5 @@ while True:
         add_user(users)
     if choose == '3':
         remove_user(users)
+    if choose == '4':
+        update_user(users)
